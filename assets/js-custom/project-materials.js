175231bfe63e252107cf375ca7f500ab8de2aa74
Tegar Darmawan <tegardar@gmail.com>
2024-05-31 13:24:51 +0700

    penyesuaian kembali pada menu project materials di sisi server untuk bisa generate pdf berdasar data yang ditampilkan pada table project-material

--- assets/js-custom/project-materials.js
+++ assets/js-custom/project-materials.js
@@ -55,18 +55,55 @@ function get_data() {
 						{ data: "keluar1" },
 						{
 							data: "tgl",
-							// render: function (data, type, row, meta) {
-							// 	var date = new Date(data);
-							// 	var formattedDate =
-							// 		date.getDate().toString().padStart(2, "0") +
-							// 		"-" +
-							// 		(date.getMonth() + 1).toString().padStart(2, "0") +
-							// 		"-" +
-							// 		date.getFullYear();
-							// 	return formattedDate;
-							// },
 						},
 					],
+				});
+				var formData = new FormData();
+				formData.append("nota", document.getElementById("nota").textContent);
+				formData.append("namac", document.getElementById("namac").textContent);
+				formData.append(
+					"project",
+					document.getElementById("project").textContent
+				);
+
+				var dataTable = $("#my-table").DataTable();
+				var tableData = dataTable.rows().data();
+				var dataToSend = [];
+
+				$.each(tableData, function (index, rowData) {
+					var date = new Date(rowData.tgl);
+					var formattedDate =
+						date.getDate().toString().padStart(2, "0") +
+						"/" +
+						(date.getMonth() + 1).toString().padStart(2, "0") +
+						"/" +
+						date.getFullYear();
+					var rowDataObject = {
+						kodeBarang: rowData.kodeb,
+						namaBarang: rowData.namab,
+						keluar: rowData.keluar,
+						satuan: rowData.satuan,
+						keterangan: rowData.keluar1,
+						tanggal: formattedDate,
+					};
+					dataToSend.push(rowDataObject);
+				});
+
+				formData.append("tableData", JSON.stringify(dataToSend));
+
+				$.ajax({
+					type: "POST",
+					url: base_url + _controller + "/save_session_data",
+					data: formData,
+					dataType: "json",
+					contentType: false,
+					processData: false,
+					success: function (response) {
+						console.log(response);
+					},
+					error: function (xhr, status, error) {
+						console.error(xhr.responseText);
+					},
 				});
 			},
 			error: function (jqXHR, textStatus, errorThrown) {
@@ -94,49 +131,52 @@ function get_data() {
 }
 
 function generate() {
-	var formData = new FormData();
-	formData.append("nota", document.getElementById("nota").textContent);
-	formData.append("namac", document.getElementById("namac").textContent);
-	formData.append("project", document.getElementById("project").textContent);
-
-	var dataTable = $("#my-table").DataTable();
-	var tableData = dataTable.rows().data();
-	var dataToSend = [];
-
-	$.each(tableData, function (index, rowData) {
-		var date = new Date(rowData.tgl);
-		var formattedDate =
-			date.getDate().toString().padStart(2, "0") +
-			"/" +
-			(date.getMonth() + 1).toString().padStart(2, "0") +
-			"/" +
-			date.getFullYear();
-		var rowDataObject = {
-			kodeBarang: rowData.kodeb,
-			namaBarang: rowData.namab,
-			keluar: rowData.keluar,
-			satuan: rowData.satuan,
-			keterangan: rowData.keluar1,
-			tanggal: formattedDate,
-		};
-		dataToSend.push(rowDataObject);
+	$(document).ready(function () {
+		window.open(base_url + "Project_materials/generate_data", "_blank");
 	});
+	// var formData = new FormData();
+	// formData.append("nota", document.getElementById("nota").textContent);
+	// formData.append("namac", document.getElementById("namac").textContent);
+	// formData.append("project", document.getElementById("project").textContent);
 
-	formData.append("tableData", JSON.stringify(dataToSend));
+	// var dataTable = $("#my-table").DataTable();
+	// var tableData = dataTable.rows().data();
+	// var dataToSend = [];
 
-	$.ajax({
-		type: "POST",
-		url: base_url + _controller + "/save_session_data",
-		data: formData,
-		dataType: "json",
-		contentType: false,
-		processData: false,
-		success: function (response) {
-			console.log(response);
-			window.open(base_url + "Project_materials/generate_data", "_blank");
-		},
-		error: function (xhr, status, error) {
-			console.error(xhr.responseText);
-		},
-	});
+	// $.each(tableData, function (index, rowData) {
+	// 	var date = new Date(rowData.tgl);
+	// 	var formattedDate =
+	// 		date.getDate().toString().padStart(2, "0") +
+	// 		"/" +
+	// 		(date.getMonth() + 1).toString().padStart(2, "0") +
+	// 		"/" +
+	// 		date.getFullYear();
+	// 	var rowDataObject = {
+	// 		kodeBarang: rowData.kodeb,
+	// 		namaBarang: rowData.namab,
+	// 		keluar: rowData.keluar,
+	// 		satuan: rowData.satuan,
+	// 		keterangan: rowData.keluar1,
+	// 		tanggal: formattedDate,
+	// 	};
+	// 	dataToSend.push(rowDataObject);
+	// });
+
+	// formData.append("tableData", JSON.stringify(dataToSend));
+
+	// $.ajax({
+	// 	type: "GET",
+	// 	url: base_url + _controller + "/save_session_data",
+	// 	data: formData,
+	// 	dataType: "json",
+	// 	contentType: false,
+	// 	processData: false,
+	// 	success: function (response) {
+	// 		console.log(response);
+	// 		window.open(base_url + "Project_materials/generate_data", "_blank");
+	// 	},
+	// 	error: function (xhr, status, error) {
+	// 		console.error(xhr.responseText);
+	// 	},
+	// });
 }
